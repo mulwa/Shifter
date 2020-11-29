f991a9435cf513d47373abb0b375d00d1933c284
Mulwa <mulwatech@gmail.com>
2020-11-29 16:41:49 +0300

    adds color constants

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/material.dart';
+
 const MAPKEY = "AIzaSyCrUAOBai695_T7qrXL75koHmIUqXXAR7I";
+
+const Color colorPrimary = Color(0xFF6a1b9a);
+const Color colorPrimaryDark = Color(0xFF38006b);
+const textColor = Colors.white;
+const Color inputBackground = Color(0xFFf9f5e5);
+const Color hintColor = Color(0xFF6E798C);
